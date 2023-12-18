59c8aa99f931d1bec90a853bc84b05ea60b9413a
Oleksii <alekseishevchenko8@gmail.com>
2023-12-18 19:50:30 +0200

    clean-folder

---
+++ sorter/clean_folder/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_folder',
+    version='1.1',
+    description='Very strange code',
+    url='https://github.com/0leksiishevchenko/sorter',
+    author='Oleksii Shevchenko',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'clean-folder = clean_folder.clean_folder.clean:main',
+        ],
+    },
+)
